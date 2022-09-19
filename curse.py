faab51daadea96d38c01c7877643774a1129ed4a
AwdeDarkar <darkardengeno@gmail.com>
2022-09-19 10:29:49 -0400

    Separate out utilities for reuse

--- curse.py
+++ curse.py
@@ -6,6 +6,8 @@ import pdb
 from types import SimpleNamespace
 from html.parser import HTMLParser
 
+from utils import BiDict, map_string, HTMLBuilder
+
 print("Running curse")
 
 """ References
@@ -28,35 +30,6 @@ Quote            '"' | \u05F2  | "ײ"
 ㅤㅤᐸspanᐳHelloㅤWorldᐸ𐤕spanᐳ
 ᐸ𐤕pᐳ
 """
-
-class BiDict(dict):
-    """
-    Bi-directional dictionary
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self._inverse = {v: k for k, v in self.items()}
-
-    def __setitem__(self, key, value):
-        super().__setitem__(key, value)
-        self._inverse[value] = key
-    
-    def __delitem__(self, key):
-        super().__delitem__(key)
-        del self._inverse[self[key]]
-    
-    @property
-    def inverse(self):
-        return self._inverse
-
-
-def map_string(string, dict):
-    """
-    Replace characters in a string according to a mapping dictionary
-    """
-    return "".join(dict.get(c, c) for c in string)
-
 class Parser(HTMLParser):
     """ Build an HTML Parse Tree """
 
@@ -108,52 +81,6 @@ class Parser(HTMLParser):
             self.code += f", {aname}=\"{aval or True}\""
         self.code += ").pop()"
 
-
-class HTMLBuilder:
-    """ Function-chaining HTML builder; this is what the HTML syntax targets """
-
-    def __init__(self):
-        self._root = None
-        self._stack = []
-    
-    @property
-    def top(self):
-        if len(self._stack) == 0:
-            return None
-        return self._stack[-1]
-    
-    def tag(self, tag_name, *args, **kwargs):
-        tag = SimpleNamespace()
-        tag.args = args
-        tag.kwargs = kwargs
-        tag.name = tag_name
-        tag.children = []
-        if self._root is None:
-            self._root = tag
-        if self.top:
-            tag.parent = self.top
-            self.top.children.append(tag)
-        self._stack.append(tag)
-        return self
-    
-    def text(self, text):
-        self.top.text = text
-        return self
-    
-    def pop(self):
-        return self
-    
-    def _to_dict(self, tag):
-        return {
-            "tag": tag.name,
-            "attrib": tag.kwargs,
-            "text": tag.args[0] if len(tag.args) > 0 else None,
-            "children": [self._to_dict(c) for c in tag.children]
-        }
-    
-    def to_dict(self):
-        return self._to_dict(self._root)
-        
 
 class ErrorHijacker:
     """
---
+++ utils.py
@@ -0,0 +1,76 @@
+from types import SimpleNamespace
+
+class BiDict(dict):
+    """
+    Bi-directional dictionary
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._inverse = {v: k for k, v in self.items()}
+
+    def __setitem__(self, key, value):
+        super().__setitem__(key, value)
+        self._inverse[value] = key
+    
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        del self._inverse[self[key]]
+    
+    @property
+    def inverse(self):
+        return self._inverse
+
+
+def map_string(string, dict):
+    """
+    Replace characters in a string according to a mapping dictionary
+    """
+    return "".join(dict.get(c, c) for c in string)
+
+
+class HTMLBuilder:
+    """ Function-chaining HTML builder; this is what the HTML syntax targets """
+
+    def __init__(self):
+        self._root = None
+        self._stack = []
+    
+    @property
+    def top(self):
+        if len(self._stack) == 0:
+            return None
+        return self._stack[-1]
+    
+    def tag(self, tag_name, *args, **kwargs):
+        tag = SimpleNamespace()
+        tag.args = args
+        tag.kwargs = kwargs
+        tag.name = tag_name
+        tag.children = []
+        if self._root is None:
+            self._root = tag
+        if self.top:
+            tag.parent = self.top
+            self.top.children.append(tag)
+        self._stack.append(tag)
+        return self
+    
+    def text(self, text):
+        self.top.text = text
+        return self
+    
+    def pop(self):
+        return self
+    
+    def _to_dict(self, tag):
+        return {
+            "tag": tag.name,
+            "attrib": tag.kwargs,
+            "text": tag.args[0] if len(tag.args) > 0 else None,
+            "children": [self._to_dict(c) for c in tag.children]
+        }
+    
+    def to_dict(self):
+        return self._to_dict(self._root)
+        
